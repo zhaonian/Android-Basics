54e909cabd5767696829d0010267a2d8430284fb
Zhaonian Luan <lzn0830@gmail.com>
2019-09-08 20:26:50 -0700

    add the basic navigation graph workflow

--- NavigationComponent/app/src/main/java/io/zluan/navigationcomponent/ChooseRecipientFragment.kt
+++ NavigationComponent/app/src/main/java/io/zluan/navigationcomponent/ChooseRecipientFragment.kt
@@ -6,11 +6,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 /**
  * A simple [Fragment] subclass.
  */
-class ChooseRecipientFragment : Fragment() {
+class ChooseRecipientFragment : Fragment(), View.OnClickListener {
+
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,5 +25,17 @@ class ChooseRecipientFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_choose_recipient, container, false)
     }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.next_btn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.cancel_btn).setOnClickListener(this)
+    }
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.next_btn ->
+                navController.navigate(R.id.action_chooseRecipientFragment_to_specifyAmountFragment)
+            R.id.cancel_btn -> activity?.onBackPressed()
+        }
+    }
 }
--- NavigationComponent/app/src/main/java/io/zluan/navigationcomponent/SpecifyAmountFragment.kt
+++ NavigationComponent/app/src/main/java/io/zluan/navigationcomponent/SpecifyAmountFragment.kt
@@ -6,11 +6,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 /**
  * A simple [Fragment] subclass.
  */
-class SpecifyAmountFragment : Fragment() {
+class SpecifyAmountFragment : Fragment(), View.OnClickListener {
+
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,5 +25,18 @@ class SpecifyAmountFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_specify_amount, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.send_btn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.cancel_btn).setOnClickListener(this)
+    }
 
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.send_btn ->
+                navController.navigate(R.id.action_specifyAmountFragment_to_confirmationFragment)
+            R.id.cancel_btn -> activity?.onBackPressed()
+        }
+    }
 }
