78e7b76f0fce737c50b30a37e0345c196dc79eae
Zhaonian Luan <lzn0830@gmail.com>
2019-09-08 18:27:06 -0700

    add all the layouts

--- NavigationComponent/app/src/main/java/io/zluan/navigationcomponent/MainFragment.kt
+++ NavigationComponent/app/src/main/java/io/zluan/navigationcomponent/MainFragment.kt
@@ -17,7 +17,7 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_fragment_main, container, false)
+        return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
 
