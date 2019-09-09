003eeae296667d91df4ec4780f6377878c4cb860
Zhaonian Luan <lzn0830@gmail.com>
2019-09-08 20:20:05 -0700

    set up basic fragment nav graph

--- NavigationComponent/app/src/main/java/io/zluan/navigationcomponent/MainFragment.kt
+++ NavigationComponent/app/src/main/java/io/zluan/navigationcomponent/MainFragment.kt
@@ -6,11 +6,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 /**
  * A simple [Fragment] subclass.
  */
-class MainFragment : Fragment() {
+class MainFragment : Fragment(), View.OnClickListener {
+
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,5 +25,23 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.view_transactions_btn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.send_money_btn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.view_balance_btn).setOnClickListener(this)
+    }
 
+    // back stack is automatically handled!
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.view_transactions_btn ->
+                navController.navigate(R.id.action_mainFragment_to_viewTransactionFragment)
+            R.id.send_money_btn ->
+                navController.navigate(R.id.action_mainFragment_to_chooseRecipientFragment)
+            R.id.view_balance_btn ->
+                navController.navigate(R.id.action_mainFragment_to_viewBalanceFragment)
+        }
+    }
 }
