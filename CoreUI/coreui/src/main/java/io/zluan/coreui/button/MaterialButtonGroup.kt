c0ac92fcec889ef7dfe41500dde6d83407904c82
Zhaonian Luan <lzn0830@gmail.com>
2019-12-19 22:46:22 -0800

    start viewpager

---
+++ CoreUI/coreui/src/main/java/io/zluan/coreui/button/MaterialButtonGroup.kt
@@ -0,0 +1,17 @@
+package io.zluan.coreui.button
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+
+/**
+ * 1. Allows dynamically changing the number of buttons.
+ * 2. For [MaterialButton] only.
+ */
+class MaterialButtonGroup(
+    context: Context?,
+    attrs: AttributeSet?,
+    defStyleAttr: Int
+) : LinearLayout(context, attrs, defStyleAttr) {
+
+}
